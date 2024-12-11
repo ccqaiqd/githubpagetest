3aec2abc7041d6974ea9c19a4947c591eab16985
ccqaiqd <ccqaiqd@users.noreply.github.com>
2024-12-11 09:01:59 +0000

    deploy: cde4d055425ba5ef68e12e9dd92e4cd7fc1e2572

---
+++ js/about.2ce8b2bb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkgithubpagetest"]=self["webpackChunkgithubpagetest"]||[]).push([[594],{9220:function(t,u,e){e.r(u),e.d(u,{default:function(){return r}});var n=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=e(1656),i={},l=(0,a.A)(i,n,s,!1,null,null,null),r=l.exports}}]);
+//# sourceMappingURL=about.2ce8b2bb.js.map
